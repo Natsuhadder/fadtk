0bccc3fb16b9187061dc8190698cbcbd25bc2f1a
modantailleur <modan.tailleur@gmail.com>
2023-12-22 14:28:46 +0100

    add a force embedding calculation option

--- fadtk/fad_calculation.py
+++ fadtk/fad_calculation.py
@@ -4,14 +4,15 @@ from .fad import FrechetAudioDistance, log
 from .model_loader import *
 from .fad_batch import cache_embedding_files
 
-def calculate_fad(model_type, baseline, eval, workers=8):
+def calculate_fad(model_type, baseline, eval, workers=8, force_emb_calc=False, audio_len=4):
 
-    models = {m.name: m for m in get_all_models()}
+    models = {m.name: m for m in get_all_models(audio_len=audio_len)}
     print(models.keys())
     model = models[model_type]
+
     for d in [baseline, eval]:
         if Path(d).is_dir():
-            cache_embedding_files(d, model, workers=workers)
+            cache_embedding_files(d, model, workers=workers, force_emb_calc=force_emb_calc)
 
     fad = FrechetAudioDistance(model, audio_load_worker=workers, load_model=False)
 
